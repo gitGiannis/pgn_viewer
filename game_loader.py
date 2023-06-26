03bf9d5419b1ca47dec8c0bf0fd56a7acebbc1e9
gitGiannis <johnmoiris@gmail.com>
2023-06-26 17:12:11 +0300

    translate to eng

--- game_loader.py
+++ game_loader.py
@@ -1,82 +1,110 @@
 # -------------------------------------------------------------------------------------------------------------------- #
-# game_loader.py: περιέχει την κλάση GameLoader                                                                        #
+# game_loader.py: includes class GameLoader                                                                            #
 # -------------------------------------------------------------------------------------------------------------------- #
 from move_checking import PieceMoveChecker
 from my_exceptions import PositionReached, NoMovesFound, PossibleCorruptFile, FriendlyCapture
 
 
-class GameLoader:
+class GameLoader(PieceMoveChecker):
     """
-    "Τρέχει" το παιχνίδι από την αρχή μέχρι το τέλος, ενώ παράλληλα αποθηκεύει σε μία λίστα με λεξικά τις πληροφορίες
-    κάθε κομματιού (όνομα, θέση κλπ) για κάθε γύρο, που θα χρησιμεύσουν στη γραφική αναπαράσταση του παιχνιδιού και στην
-    προβολή των κινήσεων (μπροστά-πίσω)
-    Κάθε λεξικό περιλαμβάνει σαν κλειδί συμβολοσειρά με το όνομα του κομματιού για κάθε κομμάτι και σαν τιμή τη θέση του
-    στον πίνακα
+    Inherits from parent class PieceMoveChecker
+    "Runs" the selected game from start to finish and stores "screenshots" of the chess board for each round
+    The screenshots for each round consists of list of dictionaries with the name of each piece,
+    as well as it's row and column index
+    Due to the fact that going backwards and finding the previous move on the board is extremely difficult or even
+    impossible, this class poses a fundamental role in the app's function
+    If any mistakes in the game moves are found, an exception is raised
 
-    Ορίσματα:
-    ---------
-        list_of_moves (list):
-            λίστα με τις επεξεργασμένες κινήσεις του αγώνα
+    ...
 
-    Μέθοδοι:
+    Attributes:
+    -----------
+        round (int):
+            counter of current half move (ply)
+
+        info_dictionaries_pre_round (list):
+            list that stores the 'screenshots' of each round
+
+        check_per_round (list):
+            list that stores whether the current half move has a check or not
+
+        captures_per_round (list):
+            list that stores whether the current half move has a captured or not
+
+        captured_piece_names (dict):
+            dictionary that stores the round of a capture and the name of the captured piece
+
+    Methods:
     --------
-        next_round(self, force: bool=False):
-            μετάβαση στον επόμενο γύρο
+        next_move(self, force: bool=False):
+            continues to next move
 
-        previous_round(self, force: bool=False):
-            μετάβαση στον προηγούμενο γύρο
+        previous_move(self, force: bool=False):
+            goes back to previous move
 
         restart_game(self):
-            μετάβαση στον αρχικό γύρο
+            restarts the game
 
-        update_captured_piece_dict(self, piece_name: str):
-            ενημέρωση του λεξικού με τα αιχμαλωτισμένα κομμάτια
+        __update_captured_piece_dict(self, piece_name: str):
+            updates dictionary with captured pieces
 
-    Εξαιρέσεις:
-    ----------------
-        NoMovesFound: εάν το αρχείο δεν έχει κινήσεις ή το αρχείο δεν μπορεί να εκτελεστεί λογικά στην κλάση Gameplay
+    Raises:
+    -------
+        NoMovesFound (Exception):
+            the current game has no moves
 
-        PositionReached: εάν ξεπεραστεί το όριο κινήσεων (0 ~ μήκος λίστας κινήσεων)
+        PossibleCorruptFile (Exception):
+            game could not be processed
+
+        FriendlyCapture (Exception):
+            a piece captures a friendly piece (not legal)
     """
     def __init__(self, list_of_moves: list):
-        # αρχικοποίηση κλάσης Gameplay για το "τρέξιμο" του παιχνιδιού
-        self.gameplay = PieceMoveChecker(list_of_moves)
+        """
+        Parameters:
+        -----------
+            list_of_moves (list):
+                λίστα με τις επεξεργασμένες κινήσεις του αγώνα
 
-        # αρχικοποίηση μεταβλητών για συσσώρευση πληροφορίας -----------------------------------------------------------
-        # λίστα όπου αποθηκεύονται τα λεξικά με τα στιγμιότυπα κάθε γύρου
-        # λεξικό που θα συσσωρεύσει στα στοιχεία του:
-        # 1. κλειδί (key) συμβολοσειρά με το όνομα του κομματιού (για κάθε κομμάτι)
-        # 2. τιμή (value) τη θέση του στον πίνακα [row][col]
+        Raises:
+        -------
+            NoMovesFound (Exception):
+                the current game has no moves
+
+            PossibleCorruptFile (Exception):
+                game could not be processed
+
+            FriendlyCapture (Exception):
+                a piece captures a friendly piece (not legal)
+        """
+        # initialization of parent class PieceMoveChecker
+        super().__init__(list_of_moves)
+
+        # initialization of structures for storing information ---------------------------------------------------------
+        # list that stores the dictionaries for each round
         self.info_dictionaries_per_round = []
 
-        # λεξικό που θα συσσωρεύσει στα στοιχεία του:
-        # 1. κλειδί (key) τον γύρο στον οποίο έγινε η κατάληψη ενός κομματιού και
-        # 2. τιμή (value) το όνομα και το χρώμα του εκάστοτε κομματιού που αιχμαλωτίστηκε
-        # το λεξικό αυτό το χρησιμοποιεί η κλάση captured_piece_frame.CapturedPieceFrame για αναπαράσταση των κομματιών
-        # που έχουν αιχμαλωτιστεί σε ζωντανό χρόνο (παράλληλα με την εξέλιξη του παιχνιδιού)
+        # dictionary that stores as:
+        # key: round a captured happens
+        # value: name of the captured piece
         self.captured_piece_names = {}
 
-        # λίστα τιμών boolean για αναπαραγωγή ήχων ---------------------------------------------------------------------
-        # εάν είναι False θα παίξει ο ήχος κίνησης κομματιού, εάν είναι True θα παίξει ο ήχος κατάληψης κομματιού
-        self.captures_per_round = []
-        # προσθήκη τιμής κατάληψης κομματιού για την αρχική θέση
-        self.captures_per_round.append(False)
+        # list of booleans for sound playing
+        # if False a move sound is played, else a capture sound (first round initialized as False)
+        self.captures_per_round = [False]
 
-        # λίστα τιμών για δήλωση ματ σε βασιλιά ------------------------------------------------------------------------
-        # εάν είναι None δεν υπάρχει κάποιο μάτ,
-        # διαφορετικά οι συμβολοσειρές "w"/"b" υποδηλώνουν μάτ σε λευκό/μαύρο βασιλιά αντιστοίχως
-        self.check_per_round = []
-        # προσθήκη τιμής ματ σε βασιλιά για την αρχική θέση
-        self.check_per_round.append(None)
+        # list of booleans king mates
+        # if None no mate is active, else "w"/"b" to show which king has mate (first round initialized as None)
+        self.check_per_round = [None]
 
-        # δείκτης τρέχοντος γύρου
+        # current half move counter
         self.round = 0
 
-        # αποθήκευση πληροφοριών ---------------------------------------------------------------------------------------
-        # βοηθητική μεταβλητή
+        # 'screenshot' storing -----------------------------------------------------------------------------------------
+        # temporary list variable
         current_round = []
-        # αποθήκευση πληροφοριών για την αρχική θέση των κομματιών
-        for piece in self.gameplay.pieces:
+        # information storing for initial position
+        for piece in self.pieces:
             current_round.append({
                 "name": piece.name[:2],
                 "row": piece.row,
@@ -84,29 +112,29 @@ class GameLoader:
             })
         self.info_dictionaries_per_round.append(current_round)
 
-        if self.gameplay.moves_length == 0:
+        if self.moves_length == 0:
             raise NoMovesFound
 
-        # προσπέλαση όλων των κομματιών του πίνακα σε κάθε γύρο και αποθήκευση τον πληροφοριών -------------------------
-        for i in range(self.gameplay.moves_length):
+        # loop through each round to create the screenshot of the chess board
+        for i in range(self.moves_length):
+            # temporary list variable
             current_round = []
-            # εκτέλεση επόμενης κίνησης και προσωρινή αποθήκευση ονόματος κομματιού που αιχμαλωτίστηκε
-            captured_piece_name = self.gameplay.next_move()
+            # next move is loaded and the captured piece name is stored temporarily
+            captured_piece_name = self.load_next_move()
 
-            # η μέθοδος next_move() επέστρεψε None
+            # load_next_move() method returned None
             if captured_piece_name is None:
-                raise PossibleCorruptFile(f"{self.gameplay.round_cnt//2 + 1}. "
-                                          f"{self.gameplay.moves[self.gameplay.round_cnt]}")
+                raise PossibleCorruptFile(f"{self.round_cnt//2 + 1}. {self.moves[self.round_cnt]}")
 
-            if self.gameplay.friendly_capture:
-                raise FriendlyCapture(f"{self.gameplay.round_cnt//2 + 1}. "
-                                      f"{self.gameplay.moves[self.gameplay.round_cnt]}")
+            # a friendly capture has been made
+            if self.friendly_capture:
+                raise FriendlyCapture(f"{self.round_cnt//2 + 1}. {self.moves[self.round_cnt]}")
 
-            # ενημέρωση λεξικού captured_piece_names
-            self.update_captured_piece_dict(captured_piece_name)
+            # captured_piece_names update
+            self.__update_captured_piece_dict(captured_piece_name)
 
-            # αποθήκευση πληροφοριών κάθε κομματιού για κάθε γύρο
-            for line in self.gameplay.board:
+            # information storing for current round
+            for line in self.board:
                 for sqr in line:
                     current_round.append({
                         "name": sqr.name[:2],
@@ -115,74 +143,79 @@ class GameLoader:
                         })
             self.info_dictionaries_per_round.append(current_round)
 
-            # προσθήκη τιμής κατάληψης κομματιού (True/False) στη λίστα
-            self.captures_per_round.append(self.gameplay.capture)
-            # προσθήκη τιμής ματ σε βασιλιά στη λίστα
-            self.check_per_round.append(self.gameplay.check)
+            # captures_per_round list update
+            self.captures_per_round.append(self.capture)
+            # check_per_round list update
+            self.check_per_round.append(self.check)
 
-    def next_round(self, force: bool = False):
+    def next_move(self, force: bool = False) -> None:
         """
-        Μετάβαση στον επόμενο γύρο
-        Εκτελεί την επόμενη κίνηση μέχρι να φτάσει στον προτελευταίο γύρο
-        force=True για μετάβαση στον τελικό γύρο
+        Continues to the next move
+        If in 2nd to final move position, raises PositionReached unless force is set to True
 
-        Ορίσματα:
-        ---------
+        ...
+
+        Parameters:
+        -----------
             force (bool) default=False:
-                True για συνέχεια στον τελικό γύρο
+                set to True to access final round
 
-        Εξαίρεση:
-        ---------
-            PositionReached: εάν έχουμε φτάσει στον προτελευταίο γύρο
+        Raises:
+        -------
+            PositionReached (Exception):
+                if 2nd to final move is reached
         """
-        if self.round < self.gameplay.moves_length - 1:
+        if self.round < self.moves_length - 1:
             self.round += 1
-        else:
-            if force:
-                self.round += 1
-            else:
-                raise PositionReached
+            return
+        if force:
+            self.round += 1
+            return
+        raise PositionReached
 
-    def previous_round(self, force: bool = False):
+    def previous_move(self, force: bool = False) -> None:
         """
-        Μέθοδος για μετάβαση στον προηγούμενο γύρο
-        Εκτελεί την προηγούμενη κίνηση μέχρι να φτάσει στη δεύτερη
-        force=True για μετάβαση στην αρχική θέση
+        Goes back to the previous move
+        if in 2nd move, raises PositionReached unless force is set to True
 
-        Ορίσματα:
-        ---------
+        ...
+
+        Parameters:
+        -----------
             force (bool) default=False:
-                True για συνέχεια στον αρχικό γύρο
+                set to True to access final round
 
-        Εξαίρεση:
-        ---------
-            PositionReached: εάν έχουμε φτάσει στον 1ο γύρο
+        Raises:
+        -------
+            PositionReached (Exception):
+                if 2nd to final move is reached
         """
         if self.round > 1:
             self.round -= 1
-        else:
-            if force:
-                self.round -= 1
-            else:
-                raise PositionReached
+            return
+        if force:
+            self.round -= 1
+            return
+        raise PositionReached
 
-    def restart_game(self):
+    def restart_game(self) -> None:
         """
-        Μετάβαση στον αρχικό γύρο
+        Restarts the game
         """
         self.round = 0
 
-    def update_captured_piece_dict(self, piece_name: str):
+    def __update_captured_piece_dict(self, piece_name: str) -> None:
         """
-        Ενημέρωση του λεξικού με τα αιχμαλωτισμένα κομμάτια
-        Η μέθοδος αυτή αποθηκεύει τον γύρο στον οποίο αιχμαλωτίστηκε κάποιο κομμάτι σαν κλειδί του λεξικού και το όνομα
-        του κομματιού σαν τιμή
-        Η αποθήκευση γίνεται εφόσον αιχμαλωτίστηκε κάποιο κομμάτι (το όνομα δεν είναι κενή συμβολοσειρά)
+        Updates the dictionary that stores the captured piece's names per round
+        This method stores the round (int) in with the piece was captured as key and the piece's name as value
+        This only happened if the name is not "   ", which implies an empty square (decoy)
 
-        Ορίσματα:
-        ---------
+        ...
+
+        Parameters:
+        -----------
             piece_name (str):
-                όνομα κομματιού που αιχμαλωτίστηκε
+                name of the captured piece
         """
         if piece_name != "   ":
-            self.captured_piece_names[self.gameplay.round_cnt + 1] = piece_name[:2]
+            self.captured_piece_names[self.round_cnt + 1] = piece_name[:2]
