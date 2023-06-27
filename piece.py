0435044b82d0181f321061fa2058276bc5827ebb
gitGiannis <johnmoiris@gmail.com>
2023-06-27 17:26:36 +0300

    linting

--- piece.py
+++ piece.py
@@ -26,7 +26,8 @@ class Piece:
         col (int):
             column (0~7) on the board (used in gui.GUI)
     """
-    def __init__(self, name: str, pos: str, state=True, row=-1, col=-1):
+
+    def __init__(self, name: str, pos: str, state: bool = True, row=-1, col=-1):
         """
         Initializes the object
 
@@ -46,13 +47,13 @@ class Piece:
             col (int):
                 column (0~7) on the board (used in gui.GUI)
         """
-        self.name = name        # name of the piece (e.g. rbl= rook black left etc.)
-        self.pos = pos          # position of the piece (e.g. a8= column a, row 8 etc.)
-        self.state = state      # state of the piece (True if active, False if captured)
+        self.name = name    # name of the piece (e.g. rbl= rook black left etc.)
+        self.pos = pos      # position of the piece (e.g. a8= column a, row 8 etc.)
+        self.state = state  # state of the piece (True if active, False if captured)
 
         # coordinates used in the graphical representation of the pieces on the board (see class gui.GUI)
-        self.row = row          # row (0~7) on the board (used in gui.GUI)
-        self.col = col          # column (0~7) on the board (used in gui.GUI)
+        self.row = row  # row (0~7) on the board (used in gui.GUI)
+        self.col = col  # column (0~7) on the board (used in gui.GUI)
 
     def __repr__(self):
         """
